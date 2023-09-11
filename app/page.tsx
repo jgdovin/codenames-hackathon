1133c3b9f66961df5363431977f22bb7e21678fa
Josh Gdovin <joshgdovin@gmail.com>
2023-09-10 23:12:09 -0600

    rename

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useRef, useState, useEffect } from "react"
 
-const page = () => {
+const Page = () => {
   const ref = useRef(null)
   const [isEditing, setIsEditing] = useState(false);
   const [nickname, setNickname] = useState('');
@@ -28,11 +28,11 @@ const page = () => {
         <div className='bg-slate-700 w-96 h-36 rounded-xl flex flex-col justify-center items-center gap-6'>
             { isEditing || !nickname ? <div><input type="text" placeholder='set your nickname' defaultValue={nickname} className='text-black' ref={ref} /> <button onClick={() => updateNickname()}>Send</button></div> : 
             <div><span className='mr-4'>{nickname}</span> <button onClick={() => setIsEditing(true)}>Edit</button></div> }
-            { nickname ? <div className='text-white'><button className='bg-blue-500 hover:bg-blue-700 text-slate-100 font-bold py-2 px-4 rounded'>Start New Game</button></div> : null }
+            { nickname ? <div className='text-white'><button className='bg-blue-500 hover:bg-blue-700 text-slate-100 font-bold py-2 px-4 rounded' onClick={() => window.location.href = '/game/main'}>Start New Game</button></div> : null }
         </div>
       </div>
     </div>
   )
 }
 
-export default page
+export default Page
