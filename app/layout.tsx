2a3beb3647df0678e721dd661d0b63e32fda98b3
Josh Gdovin <joshgdovin@gmail.com>
2023-09-10 20:28:10 -0600

    try this yay

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+          <ClerkProvider>
             <ConvexClientProvider>{children}</ConvexClientProvider>
+          </ClerkProvider>
       </body>
     </html>
   )
