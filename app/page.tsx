400c0a83a7c67a37a825b2423ec1a1ca52f1e11c
Josh Gdovin <joshgdovin@gmail.com>
2023-09-16 23:18:23 -0600

    nickname editing better

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,78 @@
-'use client'
+'use client';
 
-import { useRef, useState, useEffect } from "react"
+import { useRef, useState, useEffect } from 'react';
 
 const Page = () => {
-  const ref = useRef(null)
+  const ref = useRef(null);
   const [isEditing, setIsEditing] = useState(false);
   const [nickname, setNickname] = useState('');
 
   useEffect(() => {
-    const nickname = localStorage.getItem('nickname')
-    const userId = localStorage.getItem('userId')
+    const nickname = localStorage.getItem('nickname');
+    const userId = localStorage.getItem('userId');
     if (nickname) {
-      setNickname(nickname)
+      setNickname(nickname);
     }
     if (!userId) {
-      //generate a user uuid and store it in local storage
-      const userId = Math.random().toString(36).substring(2, 10) + '-' + Math.random().toString(36).substring(2, 10)
+      const userId =
+        Math.random().toString(36).substring(2, 10) +
+        '-' +
+        Math.random().toString(36).substring(2, 10);
       localStorage.setItem('userId', userId);
     }
-  }, [])
+  }, []);
 
   const updateNickname = () => {
     // @ts-ignore
-    const newNickname = ref?.current?.value.substring(0, 15)
-    if (!newNickname) return
-    localStorage.setItem('nickname', newNickname)
-    setNickname(newNickname)
+    const newNickname = ref?.current?.value.substring(0, 15);
+    if (!newNickname) return;
+    localStorage.setItem('nickname', newNickname);
+    setNickname(newNickname);
     setIsEditing(false);
-  }
+  };
 
   const handleKeyDown = (e: any) => {
     if (e.key === 'Enter') {
-      updateNickname()
+      updateNickname();
     }
-  }
+  };
 
   return (
     <div>
-    <div className='w-screen h-screen flex flex-col gap-4 items-center justify-center'>
+      <div className='w-screen h-screen flex flex-col gap-4 items-center justify-center'>
         <div className='bg-slate-700 w-96 h-36 rounded-xl flex flex-col justify-center items-center gap-6'>
-            { isEditing || !nickname ? <div><input type="text" onKeyDown={handleKeyDown} placeholder='set your nickname' defaultValue={nickname} className='text-black' ref={ref} /> <button onClick={() => updateNickname()}>Set Nickname</button></div> : 
-            <div><span className='mr-4'>{nickname}</span> <button onClick={() => setIsEditing(true)}>Edit</button></div> }
-            { nickname && !isEditing && <div className='text-white'><button className='bg-blue-500 hover:bg-blue-700 text-slate-100 font-bold py-2 px-4 rounded' onClick={() => window.location.href = '/game/main'}>Join Game</button></div> }
+          {isEditing || !nickname ? (
+            <div>
+              <input
+                type='text'
+                onKeyDown={handleKeyDown}
+                placeholder='set your nickname'
+                defaultValue={nickname}
+                className='text-black'
+                ref={ref}
+              />{' '}
+              <button onClick={() => updateNickname()}>Set Nickname</button>
+            </div>
+          ) : (
+            <div>
+              <span className='mr-4'>{nickname}</span>{' '}
+              <button onClick={() => setIsEditing(true)}>Edit</button>
+            </div>
+          )}
+          {nickname && !isEditing && (
+            <div className='text-white'>
+              <button
+                className='bg-blue-500 hover:bg-blue-700 text-slate-100 font-bold py-2 px-4 rounded'
+                onClick={() => (window.location.href = '/game/main')}
+              >
+                Join Game
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
