204afc74934465b77eab96168aaeec8bdacb3316
Josh Gdovin <joshgdovin@gmail.com>
2023-09-16 22:55:56 -0600

    nickname editing is better

--- app/page.tsx
+++ app/page.tsx
@@ -22,19 +22,26 @@ const Page = () => {
 
   const updateNickname = () => {
     // @ts-ignore
-    const newNickname = ref?.current?.value
+    const newNickname = ref?.current?.value.substring(0, 15)
+    if (!newNickname) return
     localStorage.setItem('nickname', newNickname)
     setNickname(newNickname)
     setIsEditing(false);
+  }
+
+  const handleKeyDown = (e: any) => {
+    if (e.key === 'Enter') {
+      updateNickname()
+    }
   }
 
   return (
     <div>
     <div className='w-screen h-screen flex flex-col gap-4 items-center justify-center'>
         <div className='bg-slate-700 w-96 h-36 rounded-xl flex flex-col justify-center items-center gap-6'>
-            { isEditing || !nickname ? <div><input type="text" placeholder='set your nickname' defaultValue={nickname} className='text-black' ref={ref} /> <button onClick={() => updateNickname()}>Set Nickname</button></div> : 
+            { isEditing || !nickname ? <div><input type="text" onKeyDown={handleKeyDown} placeholder='set your nickname' defaultValue={nickname} className='text-black' ref={ref} /> <button onClick={() => updateNickname()}>Set Nickname</button></div> : 
             <div><span className='mr-4'>{nickname}</span> <button onClick={() => setIsEditing(true)}>Edit</button></div> }
-            { nickname ? <div className='text-white'><button className='bg-blue-500 hover:bg-blue-700 text-slate-100 font-bold py-2 px-4 rounded' onClick={() => window.location.href = '/game/main'}>Join Game</button></div> : null }
+            { nickname && !isEditing && <div className='text-white'><button className='bg-blue-500 hover:bg-blue-700 text-slate-100 font-bold py-2 px-4 rounded' onClick={() => window.location.href = '/game/main'}>Join Game</button></div> }
         </div>
       </div>
     </div>
